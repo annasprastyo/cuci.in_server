2c703016634c07c847918fee650fba29a604f585
annas9029 <annasprastyo@gmail.com>
2019-08-15 17:26:27 +0700

    COMMITE

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+// mengkoneksikan ke database
+$server = "localhost";
+$server_username = "root";
+$server_password = "";
+$database_name =  "cuci_in";
+
+$conn = new Mysqli($server, $server_username, $server_password, $database_name);
+?>
+
+
